45e6d27172b6ab2c9393cb1b7e7392b05b835d8b
kyumin1227 <kyumin12271227@gmail.com>
2025-11-16 18:17:30 +0900

    feat: UserList 엔드포인트 작성
    - 필터 적용이 가능한 UserList 엔드포인트 작성

---
+++ handlers/user-service/users.go
@@ -0,0 +1,133 @@
+package handlers
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/gin-gonic/gin"
+	common "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/common"
+	userpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/user"
+	"github.com/gsc-lab/cs25-1-bannote-api-gateway/grpc/client"
+	"github.com/gsc-lab/cs25-1-bannote-api-gateway/utils"
+)
+
+type UserResponse struct {
+	ID               string    `json:"id"`
+	UserEmail        string    `json:"user_email"`
+	UserName         string    `json:"user_name"`
+	FamilyName       string    `json:"family_name"`
+	GivenName        string    `json:"given_name"`
+	UserType         string    `json:"user_type"`
+	UserStatus       string    `json:"user_status"`
+	Bio              string    `json:"bio"`
+	ProfileImageUrl  string    `json:"profile_image_url"`
+	CreatedAt        time.Time `json:"created_at"`
+	DeletedAt        time.Time `json:"deleted_at,omitempty"`
+	UserRoles        []string  `json:"user_roles"`
+	StudentClassCode *string   `json:"student_class_code,omitempty"`
+	StudentClassName *string   `json:"student_class_name,omitempty"`
+	DepartmentCode   *string   `json:"department_code,omitempty"`
+	DepartmentName   *string   `json:"department_name,omitempty"`
+}
+
+func convertUser(u *userpb.UserDetail) UserResponse {
+	var createdAt, deletedAt time.Time
+	if u.CreatedAt != nil {
+		createdAt = u.CreatedAt.AsTime()
+	}
+	if u.DeletedAt != nil {
+		deletedAt = u.DeletedAt.AsTime()
+	}
+
+	roles := make([]string, len(u.UserRoles))
+	for i, r := range u.UserRoles {
+		roles[i] = utils.StringFromUserRole(r)
+	}
+
+	return UserResponse{
+		ID:               u.UserCode,
+		UserEmail:        u.UserEmail,
+		UserName:         u.FamilyName + " " + u.GivenName,
+		FamilyName:       u.FamilyName,
+		GivenName:        u.GivenName,
+		UserType:         utils.StringFromUserType(u.UserType),
+		UserStatus:       utils.StringFromUserStatus(u.UserStatus),
+		Bio:              u.Bio,
+		ProfileImageUrl:  u.ProfileImageUrl,
+		CreatedAt:        createdAt,
+		DeletedAt:        deletedAt,
+		UserRoles:        roles,
+		StudentClassCode: u.StudentClassCode,
+		StudentClassName: u.StudentClassName,
+		DepartmentCode:   u.DepartmentCode,
+		DepartmentName:   u.DepartmentName,
+	}
+}
+
+func convertUsers(users []*userpb.UserDetail) []UserResponse {
+	result := make([]UserResponse, len(users))
+	for i, u := range users {
+		result[i] = convertUser(u)
+	}
+	return result
+}
+
+type ListUsersRequest struct {
+	Page             int32   `form:"page"`
+	Size             int32   `form:"size"`
+	UserType         *string `form:"user_type,omitempty"`
+	UserStatus       *string `form:"user_status,omitempty"`
+	DepartmentCode   *string `form:"department_code,omitempty"`
+	StudentClassCode *string `form:"student_class_code,omitempty"`
+}
+
+func ListUsers(c *gin.Context) {
+	var request ListUsersRequest
+	userClient := client.GetUserService(c)
+
+	if err := c.ShouldBind(&request); err != nil {
+		c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid request", "details": err.Error()})
+		return
+	}
+
+	ctx := utils.ContextWithMetadata(c)
+
+	var userStatus *common.UserStatus
+	if request.UserStatus != nil {
+		userStatus = utils.ParseUserStatus(*request.UserStatus)
+		if userStatus == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user_status value"})
+			return
+		}
+	}
+
+	var userType *common.UserType
+	if request.UserType != nil {
+		userType = utils.ParseUserType(*request.UserType)
+		if userType == nil {
+			c.JSON(http.StatusBadRequest, gin.H{"error": "Invalid user_type value"})
+			return
+		}
+	}
+
+	resp, err := userClient.User.ListUsers(ctx, &userpb.ListUsersRequest{
+		Page:             request.Page,
+		Size:             request.Size,
+		Status:           userStatus,
+		Type:             userType,
+		DepartmentCode:   request.DepartmentCode,
+		StudentClassCode: request.StudentClassCode,
+	})
+
+	if err != nil {
+		utils.HandleGRPCError(c, err)
+		return
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"data":  convertUsers(resp.Users),
+		"total": resp.TotalCount,
+		"page":  resp.Page,
+		"size":  resp.Size,
+	})
+}
---
+++ routes/user-service/users.go
@@ -0,0 +1,15 @@
+package user_service
+
+import (
+	"github.com/gin-gonic/gin"
+	handlers "github.com/gsc-lab/cs25-1-bannote-api-gateway/handlers/user-service"
+	"github.com/gsc-lab/cs25-1-bannote-api-gateway/middleware"
+)
+
+func RegisterUserRoute(rg *gin.RouterGroup) {
+	user := rg.Group("/users")
+
+	{
+		user.GET("", middleware.GRPCMetadata(), handlers.ListUsers)
+	}
+}
