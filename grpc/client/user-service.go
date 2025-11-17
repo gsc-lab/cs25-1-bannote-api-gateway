767ce9983f95d0ba4542a37247fbc4cf90323a10
kyumin1227 <kyumin12271227@gmail.com>
2025-11-17 21:02:58 +0900

    chore: 도메인 이름 변경에 따른 서비스 수정

--- grpc/client/user-service.go
+++ grpc/client/user-service.go
@@ -4,7 +4,7 @@ import (
 	"fmt"
 
 	healthv1 "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/common-service/healthcheck"
-	allowedemailpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/allowedemail"
+	alloweddomainpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/alloweddomain"
 	departmentpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/department"
 	rolepb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/role"
 	studentclasspb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/user-service/studentclass"
@@ -16,12 +16,12 @@ import (
 type UserServiceClient struct {
 	conn *grpc.ClientConn
 
-	User         userpb.UserServiceClient
-	Department   departmentpb.DepartmentServiceClient
-	Role         rolepb.RoleServiceClient
-	AllowedEmail allowedemailpb.AllowedEmailServiceClient
-	StudentClass studentclasspb.StudentClassServiceClient
-	Health       healthv1.HealthClient
+	User          userpb.UserServiceClient
+	Department    departmentpb.DepartmentServiceClient
+	Role          rolepb.RoleServiceClient
+	AllowedDomain alloweddomainpb.AllowedDomainServiceClient
+	StudentClass  studentclasspb.StudentClassServiceClient
+	Health        healthv1.HealthClient
 }
 
 func NewUserServiceClient(address string) (*UserServiceClient, error) {
@@ -31,13 +31,13 @@ func NewUserServiceClient(address string) (*UserServiceClient, error) {
 	}
 
 	client := &UserServiceClient{
-		conn:         conn,
-		User:         userpb.NewUserServiceClient(conn),
-		Department:   departmentpb.NewDepartmentServiceClient(conn),
-		Role:         rolepb.NewRoleServiceClient(conn),
-		AllowedEmail: allowedemailpb.NewAllowedEmailServiceClient(conn),
-		StudentClass: studentclasspb.NewStudentClassServiceClient(conn),
-		Health:       healthv1.NewHealthClient(conn),
+		conn:          conn,
+		User:          userpb.NewUserServiceClient(conn),
+		Department:    departmentpb.NewDepartmentServiceClient(conn),
+		Role:          rolepb.NewRoleServiceClient(conn),
+		AllowedDomain: alloweddomainpb.NewAllowedDomainServiceClient(conn),
+		StudentClass:  studentclasspb.NewStudentClassServiceClient(conn),
+		Health:        healthv1.NewHealthClient(conn),
 	}
 
 	fmt.Printf("✅ Connected to user-service at %s (all domains)\n", address)
