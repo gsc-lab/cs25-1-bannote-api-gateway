ceefb5ea09bc579559ea2294b614a8805a7ce429
kyumin1227 <kyumin12271227@gmail.com>
2025-11-23 23:27:34 +0900

    feat: Schedule Service Tag 관리 엔드 포인트 작성

--- routes/routes.go
+++ routes/routes.go
@@ -3,6 +3,7 @@ package routes
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/gsc-lab/cs25-1-bannote-api-gateway/handlers"
+	schedule_service "github.com/gsc-lab/cs25-1-bannote-api-gateway/routes/schedule-service"
 	studyroom_service "github.com/gsc-lab/cs25-1-bannote-api-gateway/routes/studyroom-service"
 	user_service "github.com/gsc-lab/cs25-1-bannote-api-gateway/routes/user-service"
 )
@@ -20,4 +21,6 @@ func SetupRoutes(router *gin.RouterGroup) {
 	user_service.RegisterAllowedDomainRoutes(router)
 
 	studyroom_service.RegisterRoomRoutes(router)
+
+	schedule_service.RegisterTagsRoutes(router)
 }
