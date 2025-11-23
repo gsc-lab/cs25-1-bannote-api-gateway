5c126b2d816dce32a44b4d093fbf39edc41a2144
kyumin1227 <kyumin12271227@gmail.com>
2025-11-23 23:11:20 +0900

    feat: schedule-service client 설정

--- grpc/client/schedule-service.go
+++ grpc/client/schedule-service.go
@@ -4,14 +4,25 @@ import (
 	"fmt"
 
 	healthv1 "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/common-service/healthcheck"
+	grouppb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/group"
+	grouptagpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/group_tag"
+	schedulepb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/schedule"
+	schedulelinkpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/schedule_link"
+	tagpb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/tag"
+	usergrouppb "github.com/gsc-lab/cs25-1-bannote-api-gateway/gen/go/schedule-service/user_group"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/credentials/insecure"
 )
 
 type ScheduleServiceClient struct {
-	conn *grpc.ClientConn
-
-	Health healthv1.HealthClient
+	conn         *grpc.ClientConn
+	Tag          tagpb.TagServiceClient
+	Group        grouppb.GroupServiceClient
+	GroupTag     grouptagpb.GroupTagServiceClient
+	UserGroup    usergrouppb.UserGroupServiceClient
+	Schedule     schedulepb.ScheduleServiceClient
+	ScheduleLink schedulelinkpb.ScheduleLinkServiceClient
+	Health       healthv1.HealthClient
 }
 
 func NewScheduleServiceClient(address string) (*ScheduleServiceClient, error) {
@@ -21,8 +32,14 @@ func NewScheduleServiceClient(address string) (*ScheduleServiceClient, error) {
 	}
 
 	client := &ScheduleServiceClient{
-		conn:   conn,
-		Health: healthv1.NewHealthClient(conn),
+		conn:         conn,
+		Tag:          tagpb.NewTagServiceClient(conn),
+		Group:        grouppb.NewGroupServiceClient(conn),
+		GroupTag:     grouptagpb.NewGroupTagServiceClient(conn),
+		UserGroup:    usergrouppb.NewUserGroupServiceClient(conn),
+		Schedule:     schedulepb.NewScheduleServiceClient(conn),
+		ScheduleLink: schedulelinkpb.NewScheduleLinkServiceClient(conn),
+		Health:       healthv1.NewHealthClient(conn),
 	}
 
 	fmt.Printf("✅ Connected to schedule-service at %s (all domains)\n", address)
