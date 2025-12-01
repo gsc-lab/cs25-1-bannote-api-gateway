765497bd7170a1e08d8e6ff8df1f4f9a86e44b6f
kyumin1227 <kyumin12271227@gmail.com>
2025-12-01 17:56:54 +0900

    feat: 유저 검색 엔드포인트 구현

--- routes/user-service/users.go
+++ routes/user-service/users.go
@@ -11,6 +11,7 @@ func RegisterUserRoute(rg *gin.RouterGroup) {
 
 	{
 		user.GET("", middleware.GRPCMetadata(), handlers.ListUsers)
+		user.GET("search", middleware.GRPCMetadata(), handlers.SearchUser)
 		user.POST("register", handlers.CreateUser)
 		user.PATCH(":user_code", middleware.GRPCMetadata(), handlers.UpdateUser)
 	}
